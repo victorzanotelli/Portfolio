855e7a1bbafcd6e2bac4576b119966e86810d8d8
Victor ANCLIN <victor.zano@gmail.com>
2024-02-01 12:20:39 +0100

    fixture bearly fonctionnal

--- src/DataFixtures/ClientFixtures.php
+++ src/DataFixtures/ClientFixtures.php
@@ -4,14 +4,19 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Client;
 
 class ClientFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+        public function load(ObjectManager $manager): void
+        {
+        for ($i = 1; $i <= 20; $i++) {
+            $client = new Client();
+            $client->setName( 'Client_' . $i);
+            $this->addReference('Client_' . $i, $client);
+            $manager->persist($client);
 
+        }
         $manager->flush();
     }
 }
--- src/DataFixtures/TechnologyFixtures.php
+++ src/DataFixtures/TechnologyFixtures.php
@@ -2,16 +2,28 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Technology;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class TechnologyFixtures extends Fixture
 {
+
+    public const TECHNOLOGY = [
+        'Peinture',
+        'Sculpture',
+        'Photo',
+        'FanArt',
+        'IA',
+    ];
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        foreach (self::TECHNOLOGY as $technoName) {
+            $techno = new Technology();
+            $techno->setName($technoName);
+            $manager->persist($techno);
+            $this->addReference('Techno_' . $technoName, $techno);
+        }
         $manager->flush();
     }
 }
