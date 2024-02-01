c00160de8ad85a6808a0e281574f43d22cd27e4b
Victor ANCLIN <victor.zano@gmail.com>
2024-02-01 15:47:57 +0100

    develop was doomed (bug php.ini file)

---
+++ src/Form/ProjectType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use App\Entity\Project;
+use App\Entity\Technology;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('gitHubLink')
+            ->add('picture')
+            ->add('date')
+            ->add('technology', EntityType::class, [
+                'class' => Technology::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+'choice_label' => 'id',
+            ])
+            ->add('client', EntityType::class, [
+                'class' => Client::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+        ]);
+    }
+}
